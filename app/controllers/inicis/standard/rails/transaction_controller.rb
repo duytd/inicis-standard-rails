e7b955e8d394641d85178c6d8b64aeda6633b477
Trinh Duc Duy <duytd.hanu@gmail.com>
2015-12-14 00:55:56 +0700

    Inicis callback handler

--- app/controllers/inicis/standard/rails/transaction_controller.rb
+++ app/controllers/inicis/standard/rails/transaction_controller.rb
@@ -1,18 +1,20 @@
 require "inicis/standard/rails/payment"
+require "inicis/standard/rails/authentication"
 
 module Inicis
   module Standard
     module Rails
       class TransactionController < ::ApplicationController
+        before_action :load_logger
         def pay
-          order = {
-            id: "order_1",
-            goods_name: "Good Name",
-            buyer_name: "Trinh Duc Duy",
-            buyer_email: "duytd.hanu@gmail.com",
-            buyer_phone: "0123456789",
-            price: 10000
-          }
+          # order = {
+          #   id: "order_1",
+          #   goods_name: "Good Name",
+          #   buyer_name: "Buyer Name",
+          #   buyer_email: "buyer@shopstory.co.kr",
+          #   buyer_phone: "0123456789",
+          #   price: 10000
+          # }
 
           inicis_payment = Inicis::Standard::Rails::Payment.new order: order
           @request_payload = inicis_payment.generate_payload
@@ -25,6 +27,48 @@ module Inicis
         end
 
         def callback
+          @logger.info "Callback from Inicis..."
+          @logger.debug params
+          inicis_authentication = Inicis::Standard::Rails::Authentication.new
+
+          if params[:resultCode] == "0000"
+            @logger.info "Successful make payment request. Waiting for approvement..."
+
+            data = {
+              mid: params[:mid],
+              token: params[:authToken],
+              authorize_url: params[:authUrl],
+              net_cancel_url: params[:netCancelUrl],
+              ack_url: params[:ackUrl]
+            }
+
+            approvement = inicis_authentication.approve data
+
+            if approvement
+              @logger.info "Payment is approved successfully"
+              #To_do: save_transaction
+              #To_do: success
+            else
+              @logger.debug "Failed to approve payment"
+              failure "Approvement Failed !"
+            end
+          else
+            @logger.debug "Failed to make payment request"
+            failure "Invalid Request !"
+          end
+        end
+
+        def failure message
+          flash[:danger] = message
+          render :pay
+        end
+
+        def success
+        end
+
+        private
+        def load_logger
+          @logger ||= Logger.new "#{::Rails.root}/log/inicis.log"
         end
       end
     end
---
+++ lib/inicis/standard/rails/authentication.rb
@@ -0,0 +1,65 @@
+require "rjb"
+
+module Inicis
+  module Standard
+    module Rails
+      class Authentication
+        def initialize
+          jars = Dir.glob("#{File.dirname(__FILE__)}/java/*.jar").join ":"
+          Rjb.load jars, ["-Xmx512M"]
+
+          @hash_map = Rjb::import "java.util.HashMap"
+          @hash_table = Rjb::import "java.util.HashTable"
+          @signature_util = Rjb::import "com.inicis.std.util.SignatureUtil"
+          @http_util = Rjb::import "com.inicis.std.util.HttpUtil"
+          @parse_util = Rjb::import "com.inicis.std.util.ParseUtil"
+        end
+      end
+
+      def approve data
+        sign_params = @hash_map.new
+        sign_params.put "authToken", data.token
+        sign_params.put "timestamp", timestamp
+
+        signature = signature_util.makeSignature sign_params
+
+        hash = @hash_table.new
+        hash.put "mid", data[:mid]
+        hash.put "authToken", data[:token]
+        hash.put "signature", signature
+        hash.put "timestamp", @signature_util.getTimestamp
+        hash.put "charset", "UTF-8"
+        hash.put "format", "JSON"
+        hash.put "price", price
+
+        result_str = @http_util.processHTTP hash, data[:authorize_url]
+        result_str = result_str.replace(",", "&").replace(":", "=").replace("\"", "").replace(" ","").replace("\n", "").replace("}", "").replace("{", "")
+        result_map = parse_util.parseStringToMap result_str
+
+        if result_map.get("resultCode") == "0000"
+          check_map = @hash_table.new
+          check_map.put "mid", data[:mid]
+          check_map.put "authToken", data[:token]
+          check_map.put "signature", signature
+          check_map.put "timestamp", @signature_util.getTimestamp
+          check_map.put "applDate", result_map.get("applDate")
+          check_map.put "applTime", result_map.get("applTime")
+          check_map.put "charset", "UTF-8"
+          check_map.put "format", "JSON"
+
+          ack_result_str = @http_util.processHTTP checkMap, data[:ack_url]
+          ack_map = @hash_map.new
+          ackMap = @parse_util.parseStringToMap ack_result_str
+
+          return map_to_hash result_map
+        end
+
+        nil
+      end
+
+      def self.map_to_hash map
+         Hash[map.toString[1..-2].split(",").collect{|x| x.strip.split("=")}]
+      end
+    end
+  end
+end
